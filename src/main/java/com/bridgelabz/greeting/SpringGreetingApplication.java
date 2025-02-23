a4d97c8ccac9e1c6ae509d1555a8e385649564f7
Yaman Mahtha <yamanmahtha@gmail.com>
2025-02-23 14:51:59 +0530

    [Yaman] Project Created

---
+++ src/main/java/com/bridgelabz/greeting/SpringGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGreetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/greeting/SpringGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
